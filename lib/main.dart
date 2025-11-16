11b0bb280831d23fe41f41cd99c1b31a25a858f4
Oscar Peace <oscar@oscarcp.net>
2025-11-16 11:29:04 +0000

    feat: change routing to go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/views/product_page.dart';
+import 'package:go_router/go_router.dart';
 import 'views/home.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: '/product',
+      builder: (context, state) => ProductPage(),
+    )
+  ]
+);
 
 void main() {
   runApp(const UnionShopApp());
@@ -11,18 +25,13 @@ class UnionShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Union Shop',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4d2963)),
       ),
-      home: const HomeScreen(),
-      // By default, the app starts at the '/' route, which is the HomeScreen
-      initialRoute: '/',
-      // When navigating to '/product', build and return the ProductPage
-      // In your browser, try this link: http://localhost:49856/#/product
-      routes: {'/product': (context) => const ProductPage()},
+      routerConfig: _router,
     );
   }
 }
--- lib/widgets/product_card_widget.dart
+++ lib/widgets/product_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ProductCard extends StatelessWidget {
   final String title;
@@ -16,7 +17,7 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(context, '/product');
+        context.go('/product');
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
