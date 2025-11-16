857b259e5aa0838a11710ae9cc8c887758703e2b
Oscar Peace <oscar@oscarcp.net>
2025-11-16 11:47:30 +0000

    feat: replace placeholder text in home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -34,7 +34,7 @@ class HomeScreen extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(vertical: 8),
                     color: const Color(0xFF4d2963),
                     child: const Text(
-                      'PLACEHOLDER HEADER TEXT',
+                      'Union Shop',
                       textAlign: TextAlign.center,
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
@@ -168,7 +168,7 @@ class HomeScreen extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         const Text(
-                          'Placeholder Hero Title',
+                          'Student Union Shop',
                           style: TextStyle(
                             fontSize: 32,
                             fontWeight: FontWeight.bold,
@@ -178,7 +178,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                         const SizedBox(height: 16),
                         const Text(
-                          "This is placeholder text for the hero section.",
+                          "Shop student union merchandise",
                           style: TextStyle(
                             fontSize: 20,
                             color: Colors.white,
