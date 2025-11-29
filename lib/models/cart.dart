7c54e16caf376edf2a9d481de24ec11dcc3822c9
Oscar Peace <oscar@oscarcp.net>
2025-11-29 11:02:48 +0000

    feat: update cart model to work with provider

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/widgets.dart';
 import 'package:union_shop/models/product.dart';
 
 class CartItem {
   final Product product;
+  final Map<String, String> attributes;
   late int _quantity;
 
-  CartItem({required this.product, required int quantity}) {
+  CartItem(
+      {required this.product,
+      required this.attributes,
+      required int quantity}) {
     _quantity = quantity;
   }
 
@@ -19,17 +24,42 @@ class CartItem {
   }
 }
 
-class Cart {
-  
-  late List<CartItem> items;
+class Cart extends ChangeNotifier {
+  late List<CartItem> _items;
 
   Cart() {
-    items = [];
+    _items = [];
   }
 
-  double get totalPrice => items.fold(
-      0.00,
-      (previousValue, item) =>
-          previousValue + (item.product.truePrice) * item.quantity);
+  CartItem getItem(int index) {
+    return _items[index];
+  }
 
+  CartItem removeItem(int index) {
+    CartItem item = _items.removeAt(index);
+    notifyListeners();
+    return item;
+  }
+
+  int getQuantity(int index) {
+    return _items[index].quantity;
+  }
+
+  void setQuantity(int index, int newQuantity) {
+    _items[index].quantity = newQuantity;
+    notifyListeners();
+  }
+
+  void clear() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  int get length => _items.length;
+  bool get isEmpty => _items.isEmpty;
+
+  double get totalPrice => _items.fold(
+      0.00,
+      (previousValue, element) =>
+          previousValue + element.product.truePrice);
 }
