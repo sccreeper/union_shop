2421c0018006546451d578d325ca5dc0b2e5b6dd
Oscar Peace <oscar@oscarcp.net>
2025-12-02 17:43:56 +0000

    feat(collections): add sorting to collection page

--- lib/views/collection_page.dart
+++ lib/views/collection_page.dart
@@ -1,50 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/models/collection.dart';
+import 'package:union_shop/models/product.dart';
 import 'package:union_shop/widgets/product_card_widget.dart';
 
-enum FilterByType {
+enum FilterBy {
   allProducts,
+  onSale,
 }
 
-enum SortByType {
-  bestSelling,
+enum SortBy {
   alphabeticallyAZ,
   alphabeticallyZA,
   priceLowHigh,
   priceHighLow,
-  dateOldNew,
-  dateNewOld,
 }
 
-class CollectionPage extends StatelessWidget {
+class CollectionPage extends StatefulWidget {
   final Collection collection;
 
   const CollectionPage({super.key, required this.collection});
 
-  List<DropdownMenuEntry<SortByType>> _buildSortByEntries() {
-    List<DropdownMenuEntry<SortByType>> entries = [];
+  @override
+  State<StatefulWidget> createState() => _CollectionPageState();
+}
 
-    for (SortByType sortBy in SortByType.values) {
-      DropdownMenuEntry<SortByType> newEntry =
-          DropdownMenuEntry(value: sortBy, label: sortBy.name);
+class _CollectionPageState extends State<CollectionPage> {
+  SortBy _sortBy = SortBy.priceLowHigh;
+  FilterBy _filterBy = FilterBy.allProducts;
 
-      entries.add(newEntry);
+  late List<Product> _productsCopy;
+
+  List<DropdownMenuItem<SortBy>> _buildSortByEntries() => SortBy.values
+      .map((v) => DropdownMenuItem(
+            value: v,
+            child: Text(v.name),
+          ))
+      .toList();
+
+  List<DropdownMenuItem<FilterBy>> _buildFilterByEntries() => FilterBy.values
+      .map((v) => DropdownMenuItem(
+            value: v,
+            child: Text(v.name),
+          ))
+      .toList();
+
+  void _filter(FilterBy filterMode) {
+    _filterBy = filterMode;
+
+    if (filterMode == FilterBy.onSale) {
+      List<Product> temp = [];
+
+      for (Product product in _productsCopy) {
+        if (product.onSale) {
+          temp.add(product);
+        }
+      }
+
+      setState(() {
+        _productsCopy = [...temp];
+      });
+    } else {
+      setState(() {
+        _productsCopy = [...widget.collection.products];
+      });
     }
-
-    return entries;
   }
 
-  List<DropdownMenuEntry<FilterByType>> _buildFilterByEntries() {
-    List<DropdownMenuEntry<FilterByType>> entries = [];
+  void _sort(SortBy sortMode) {
 
-    for (FilterByType filterBy in FilterByType.values) {
-      DropdownMenuEntry<FilterByType> newEntry =
-          DropdownMenuEntry(value: filterBy, label: filterBy.name);
+    _sortBy = sortMode;
 
-      entries.add(newEntry);
-    }
+    _productsCopy.sort((a,b) {
+      switch (_sortBy) {
+        case SortBy.priceLowHigh:
+          return a.truePrice.compareTo(b.truePrice);
+        case SortBy.priceHighLow:
+          return b.truePrice.compareTo(a.truePrice);
+        case SortBy.alphabeticallyAZ:
+          return a.name.compareTo(b.name);
+        case SortBy.alphabeticallyZA:
+          return b.name.compareTo(a.name);
+        default:
+          return 0;
+      }
+    });
 
-    return entries;
+    setState(() {});
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _productsCopy = [...widget.collection.products];
+    _sort(_sortBy);
   }
 
   @override
@@ -54,57 +104,60 @@ class CollectionPage extends StatelessWidget {
       child: Column(
         children: [
           // Header
-          Container(
-            decoration:
-                const BoxDecoration(border: Border(bottom: BorderSide())),
-            child: Column(
-              children: [
-                Text(
-                  collection.title,
-                  style: Theme.of(context).textTheme.headlineLarge,
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                // Filter/inputs row
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    spacing: 4.0,
-                    children: [
-                      SizedBox(
-                        width: 150,
-                        child: DropdownMenu(
-                          dropdownMenuEntries: _buildFilterByEntries(),
-                          label: const Text("Filter By"),
-                        ),
+          Column(
+            children: [
+              Text(
+                widget.collection.title,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              // Filter/inputs row
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 4.0,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      child: DropdownButtonFormField<FilterBy>(
+                        onChanged: (value) {_filter(value ?? FilterBy.allProducts);},
+                        initialValue: _filterBy,
+                        items: _buildFilterByEntries(),
+                        hint: const Text("Filter By"),
                       ),
-                      SizedBox(
-                        width: 150,
-                        child: DropdownMenu(
-                          dropdownMenuEntries: _buildSortByEntries(),
-                          label: const Text("Sort By"),
-                        ),
+                    ),
+                    SizedBox(
+                      width: 150,
+                      child: DropdownButtonFormField<SortBy>(
+                        onChanged: (value) {_sort(value ?? SortBy.priceLowHigh);},
+                        initialValue: _sortBy,
+                        items: _buildSortByEntries(),
+                        hint: const Text("Sort By"),
                       ),
-                      const Text("n Products")
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    ),
+                    Text("${_productsCopy.length} Products")
+                  ],
+                ),
+              )
+            ],
           ),
           const SizedBox(
             height: 4.0,
           ),
           GridView.count(
-            primary: false,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            children: collection.products.map((v) => ProductCard(product: v,)).toList()
-          )
+              primary: false,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: _productsCopy
+                  .map((v) => ProductCard(
+                        product: v,
+                      ))
+                  .toList())
         ],
       ),
     );
