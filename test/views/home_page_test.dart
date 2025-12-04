21f253094ceb95d029c59af3cd70d7d54ac29e2e
Oscar Peace <oscar@oscarcp.net>
2025-12-04 20:45:43 +0000

    tests: add home page tests

---
+++ test/views/home_page_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:union_shop/layouts/base_layout.dart';
+import 'package:union_shop/models/cart.dart';
+import 'package:union_shop/repositories/collection_repository.dart';
+import 'package:union_shop/repositories/product_repository.dart';
+import 'package:union_shop/views/home.dart';
+import 'package:union_shop/widgets/product_card_widget.dart';
+
+void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    await ProductRepository.instance.loadProducts();
+    await CollectionRepository.instance.loadCollections();
+  });
+
+  testWidgets("home page displays correctly", (tester) async {
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => Cart(),
+        child: const MaterialApp(
+          home: BaseLayout(child: HomeScreen()),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text("All Products"), findsOne);
+
+    expect(tester.widgetList(find.byType(ProductCard)).length,
+        ProductRepository.instance.productEntries.length);
+  });
+}
