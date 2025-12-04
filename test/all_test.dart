0849f7fa5e6e376154bbc6f9acadd965bdb96e76
Oscar Peace <oscar@oscarcp.net>
2025-12-04 20:09:54 +0000

    tests: add imports file to get around coverage issue

---
+++ test/all_test.dart
@@ -0,0 +1,32 @@
+// Test coverage won't include unused files in report
+// See: https://github.com/flutter/flutter/issues/27997
+
+// ignore_for_file: unused_import
+import 'package:union_shop/layouts/base_layout.dart';
+
+import "package:union_shop/models/cart.dart";
+import "package:union_shop/models/collection.dart";
+import "package:union_shop/models/product.dart";
+
+import "package:union_shop/repositories/collection_repository.dart";
+import 'package:union_shop/repositories/product_repository.dart';
+
+import "package:union_shop/util/slices.dart";
+
+import "package:union_shop/views/about.dart";
+import 'package:union_shop/views/cart_page.dart';
+import 'package:union_shop/views/collection_page.dart';
+import 'package:union_shop/views/collections_page.dart';
+import 'package:union_shop/views/error_page.dart';
+import 'package:union_shop/views/home.dart';
+import 'package:union_shop/views/login_page.dart';
+import 'package:union_shop/views/product_page.dart';
+import 'package:union_shop/views/search_page.dart';
+
+import 'package:union_shop/widgets/collection_card.dart';
+import 'package:union_shop/widgets/header_slideshow.dart';
+import 'package:union_shop/widgets/paginated.dart';
+import 'package:union_shop/widgets/product_card_widget.dart';
+import 'package:union_shop/widgets/slideshow_widget.dart';
+
+void main() {}
